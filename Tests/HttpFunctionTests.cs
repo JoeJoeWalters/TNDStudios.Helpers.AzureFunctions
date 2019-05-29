1806f5a581845dad4ad38f3e91e0aa4cf6316d5f
TNDStudios <info@thenakeddeveloper.com>
2019-05-29 21:42:16 +0100

    Added green path test

--- Tests/HttpFunctionTests.cs
+++ Tests/HttpFunctionTests.cs
@@ -14,7 +14,7 @@ namespace Tests
     public class HttpFunctionTests
     {
         [Fact]
-        public void Example()
+        public void Bad_Request_If_No_Name_Given()
         {
             // Arrange
             ILogger logger = TestLoggerFactory.CreateLogger();
@@ -29,6 +29,23 @@ namespace Tests
             // Assert
             Assert.Equal(HttpStatusCode.BadRequest, result.StatusCode);
             Assert.Equal("Please pass a name on the query string or in the request body", result.Value);
+        }
+
+        [Fact]
+        public void Green_Path()
+        {
+            // Arrange
+            ILogger logger = TestLoggerFactory.CreateLogger();
+            IBinder binder = TestBinderFactory.CreateBinder();
+            DefaultHttpRequest httpRequest = TestHttpFactory.CreateHttpRequest("name", "Joe");
+
+            // Act
+            HttpResult result = TestHttpFactory.GetHttpResult(
+                HttpFunction.Run(httpRequest, binder, logger).Result
+                );
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, result.StatusCode);
         }
     }
 }
