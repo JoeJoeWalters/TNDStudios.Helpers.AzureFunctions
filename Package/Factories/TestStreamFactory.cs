8e89062684750b3a53c8bef3679693bd63ff6714
TNDStudios <info@thenakeddeveloper.com>
2019-05-31 23:31:47 +0100

    Creating test streams with data loaded from embedded resources

--- Package/Factories/TestStreamFactory.cs
+++ Package/Factories/TestStreamFactory.cs
@@ -1,14 +1,51 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 using System.Text;
 using TNDStudios.Helpers.AzureFunctions.Testing.Mocks;
 
 namespace TNDStudios.Helpers.AzureFunctions.Testing.Factories
 {
+    public enum TestStreamType
+    {
+        Content,
+        EmbeddedResource
+    }
+
     public static class TestStreamFactory
     {
-        public static TestStream CreateStream() => CreateStream(Encoding.UTF8);
-        public static TestStream CreateStream(Encoding encoding) => CreateStream(encoding, String.Empty);
-        public static TestStream CreateStream(Encoding encoding, String content)
-            => new TestStream(encoding, content);
+        public static TestStream CreateStream(TestStreamType type) => CreateStream(type, Encoding.UTF8, String.Empty);
+        public static TestStream CreateStream(TestStreamType type, Encoding encoding) => CreateStream(type, encoding, String.Empty);
+        public static TestStream CreateStream(TestStreamType type, Encoding encoding, String loadFrom)
+        {
+            String value = String.Empty;
+
+            switch (type)
+            {
+                case TestStreamType.EmbeddedResource:
+
+                    using (Stream resourceStream = Assembly
+                            .GetCallingAssembly()
+                            .GetManifestResourceStream(loadFrom))
+                    {
+                        using (StreamReader sr = new StreamReader(resourceStream, encoding))
+                        {
+                            value = sr.ReadToEnd();
+                        }
+                    }
+
+                    break;
+
+                default:
+
+                    value = loadFrom;
+
+                    break;
+            }
+
+            TestStream result = new TestStream(encoding, value);
+            
+            return result;
+        }
     }
 }
--- Tests/BlobFunctionTests.cs
+++ Tests/BlobFunctionTests.cs
@@ -20,9 +20,11 @@ namespace Tests
             // Arrange
             ILogger logger = TestLoggerFactory.CreateLogger();
             String name = "name of the blob";
-            TestStream triggerBlob = TestStreamFactory.CreateStream(Encoding.UTF8, "Test Data");
-            TestStream outputBlob1 = TestStreamFactory.CreateStream();
-            TestStream outputBlob2 = TestStreamFactory.CreateStream();
+            TestStream triggerBlob = TestStreamFactory.CreateStream(TestStreamType.EmbeddedResource,
+                                                                    Encoding.UTF8,
+                                                                    "Tests.Data.Files.TestFile.xml");
+            TestStream outputBlob1 = TestStreamFactory.CreateStream(TestStreamType.Content);
+            TestStream outputBlob2 = TestStreamFactory.CreateStream(TestStreamType.Content);
 
             // Act
             BlobFunction.Run(triggerBlob, name, outputBlob1, outputBlob2, logger);
