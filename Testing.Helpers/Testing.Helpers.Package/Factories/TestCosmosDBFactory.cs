218f456025affb6571d30ad52fb76efcd8329371
TNDStudios <info@thenakeddeveloper.com>
2019-06-17 00:43:00 +0100

    Test document client initialiser for test data and fix for test so that the mocked Create Async call actually performs. Mostly just messing around to get an idea of the scope for NSubstitute mocking of the client via the factory.

--- Testing.Helpers/Testing.Helpers.Package/Factories/TestCosmosDBFactory.cs
+++ Testing.Helpers/Testing.Helpers.Package/Factories/TestCosmosDBFactory.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
 using NSubstitute;
 using System;
 using System.Collections.Generic;
@@ -13,14 +14,18 @@ namespace TNDStudios.Helpers.AzureFunctions.Testing.Factories
         /// Get test Document Client for Cosmos DB
         /// </summary>
         /// <returns></returns>
-        public static IDocumentClient CreateDocumentClient()
+        public static IDocumentClient CreateDocumentClient() => CreateDocumentClient(new List<Document>() { });
+        public static IDocumentClient CreateDocumentClient(List<Document> initialisingDocuments)
         {
             IDocumentClient client = Substitute.For<IDocumentClient>();
-            client.CreateDocumentAsync("", null)
-                .WhenForAnyArgs(call => 
-                {
-
-                });
+            client
+                .CreateDocumentAsync(Arg.Any<string>(), Arg.Any<object>())
+                .Returns(callInfo => 
+                    {
+                        Document resource = new Document();
+                        initialisingDocuments.Add(resource);
+                        return new ResourceResponse<Document>(resource);
+                    });
 
             return client;
         }
