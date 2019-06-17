c1eaa2a3e49ba37d884c092bfca78732653b11ab
TNDStudios <info@thenakeddeveloper.com>
2019-06-18 00:11:35 +0100

    Slow progress on the Document Client mocking

--- Testing.Helpers/Testing.Helpers.Package/Factories/TestCosmosDBFactory.cs
+++ Testing.Helpers/Testing.Helpers.Package/Factories/TestCosmosDBFactory.cs
@@ -4,27 +4,65 @@ using NSubstitute;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 using TNDStudios.Helpers.AzureFunctions.Testing.Mocks;
 
 namespace TNDStudios.Helpers.AzureFunctions.Testing.Factories
 {
+    /// <summary>
+    /// Definition of how a test document client should behave
+    /// </summary>
+    public class DocumentClientTestPolicy
+    {
+        /// <summary>
+        /// Exceptions to raise when actions happen
+        /// </summary>
+        public Exception ReadException { get; set; }
+        public Exception WriteException { get; set; }
+    }
+
     public static class TestCosmosDBFactory
     {
         /// <summary>
         /// Get test Document Client for Cosmos DB
         /// </summary>
         /// <returns></returns>
-        public static IDocumentClient CreateDocumentClient() => CreateDocumentClient(new List<Document>() { });
-        public static IDocumentClient CreateDocumentClient(List<Document> initialisingDocuments)
+        public static IDocumentClient CreateDocumentClient(List<Document> initialisingDocuments, DocumentClientTestPolicy policy)
         {
             IDocumentClient client = Substitute.For<IDocumentClient>();
+
             client
-                .CreateDocumentAsync(Arg.Any<string>(), Arg.Any<object>())
+                .CreateDocumentAsync(
+                    Arg.Any<string>(), 
+                    Arg.Any<object>()
+                    )
                 .Returns(callInfo => 
                     {
+                        // If an exception should be raised when trying to write
+                        // a document to Cosmos
+                        if (policy.WriteException != null)
+                            throw policy.WriteException;
+
                         Document resource = new Document();
+                        //resource.LoadFrom(); callInfo.ArgAt<object>[0]
                         initialisingDocuments.Add(resource);
                         return new ResourceResponse<Document>(resource);
+                    });
+
+            client
+                .ReadDocumentAsync(
+                    Arg.Any<string>(), 
+                    Arg.Any<RequestOptions>(), 
+                    Arg.Any<CancellationToken>()
+                    )
+                .Returns(callInfo => 
+                    {
+                        // If an exception should be raised when trying to read
+                        // a document from Cosmos
+                        if (policy.ReadException != null)
+                            throw policy.ReadException;
+
+                        return new ResourceResponse<Document>();
                     });
 
             return client;
--- Testing.Helpers/Testing.Helpers.Tests/CosmosDBFunctionTests.cs
+++ Testing.Helpers/Testing.Helpers.Tests/CosmosDBFunctionTests.cs
@@ -1,6 +1,7 @@
 ﻿using Functions;
 using Microsoft.Azure.Documents;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using TNDStudios.Helpers.AzureFunctions.Testing.Factories;
 using TNDStudios.Helpers.AzureFunctions.Testing.Mocks;
@@ -17,7 +18,13 @@ namespace Tests
             ILogger logger = TestLoggerFactory.CreateLogger();
             List<Document> documents = new List<Document>() { new Document() };
             TestAsyncCollector<Document> output = TestCollectorFactory.CreateAsyncCollector<Document>();
-            IDocumentClient documentClient = TestCosmosDBFactory.CreateDocumentClient();
+            IDocumentClient documentClient = TestCosmosDBFactory
+                .CreateDocumentClient(documents, 
+                    new DocumentClientTestPolicy()
+                    {
+                        ReadException = new Exception() { },
+                        WriteException = new Exception() { }
+                    });
 
             // Act
             CosmosDBFunction.Run(documents, output, documentClient, logger);
